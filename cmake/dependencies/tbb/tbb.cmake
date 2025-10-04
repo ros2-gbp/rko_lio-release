c68a38de6d00ef4547ea979a11a9080cee5b6fd2
Meher Malladi <rm.meher97@gmail.com>
2025-10-04 13:37:58 +0200

    Rebase from 'upstream'

--- cmake/dependencies/tbb/tbb.cmake
+++ cmake/dependencies/tbb/tbb.cmake
@@ -7,5 +7,5 @@ option(TBB_TEST OFF)
 FetchContent_Declare(
   TBB
   URL https://github.com/uxlfoundation/oneTBB/archive/refs/tags/v2022.2.0.tar.gz
-      SYSTEM EXCLUDE_FROM_ALL OVERRIDE_FIND_PACKAGE)
+      ${RKO_LIO_FETCHCONTENT_COMMON_FLAGS})
 FetchContent_MakeAvailable(TBB)
