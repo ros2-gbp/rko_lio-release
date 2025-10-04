5a1703dc168957088a675de29d040793c78bdb17
Meher Malladi <rm.meher97@gmail.com>
2025-10-04 13:36:28 +0200

    Rebase from 'upstream'

--- cmake/dependencies/bonxai/bonxai.cmake
+++ cmake/dependencies/bonxai/bonxai.cmake
@@ -2,11 +2,7 @@ FetchContent_Declare(
   Bonxai
   GIT_REPOSITORY https://github.com/facontidavide/Bonxai.git
   GIT_TAG 02d401b1ce38bce870c6704bcd4e35a56a641411 # sep 14 2025 master
-  SOURCE_SUBDIR
-  bonxai_core
-  SYSTEM
-  EXCLUDE_FROM_ALL
-  OVERRIDE_FIND_PACKAGE)
+  SOURCE_SUBDIR bonxai_core ${RKO_LIO_FETCHCONTENT_COMMON_FLAGS})
 FetchContent_MakeAvailable(Bonxai)
 
 if(FETCHCONTENT_FULLY_DISCONNECTED)
