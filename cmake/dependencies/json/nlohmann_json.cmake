733c8f70d0ad5e94e652df6c90de701994929935
Meher Malladi <rm.meher97@gmail.com>
2025-10-07 15:22:28 +0200

    Rebase from 'upstream'

--- cmake/dependencies/json/nlohmann_json.cmake
+++ cmake/dependencies/json/nlohmann_json.cmake
@@ -3,3 +3,5 @@ FetchContent_Declare(
   URL https://github.com/nlohmann/json/archive/refs/tags/v3.12.0.tar.gz
       ${RKO_LIO_FETCHCONTENT_COMMON_FLAGS})
 FetchContent_MakeAvailable(nlohmann_json)
+
+mock_find_package_for_older_cmake(nlohmann_json)
