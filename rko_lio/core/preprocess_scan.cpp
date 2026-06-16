297056a446f5ff47467b911f7712a5973289064d
Meher Malladi <rm.meher97@gmail.com>
2026-06-16 17:45:04 +0200

    Rebase from 'upstream'

--- rko_lio/core/preprocess_scan.cpp
+++ rko_lio/core/preprocess_scan.cpp
@@ -4,7 +4,7 @@
 namespace rko_lio::core {
 
 PreprocessingResult preprocess_scan(const Vector3dVector& frame, const LIO::Config& config) {
-  std::vector<Eigen::Vector3d> clipped_frame;
+  Vector3dVector clipped_frame;
   clipped_frame.reserve(frame.size());
 
   std::for_each(frame.cbegin(), frame.cend(), [&](const auto& point) {
@@ -16,13 +16,14 @@ PreprocessingResult preprocess_scan(const Vector3dVector& frame, const LIO::Conf
   clipped_frame.shrink_to_fit();
 
   if (config.double_downsample) {
-    const Vector3dVector downsampled_frame = voxel_down_sample(clipped_frame, config.voxel_size * 0.5);
-    const Vector3dVector keypoints = voxel_down_sample(downsampled_frame, config.voxel_size * 1.5);
-    return {.filtered_frame = clipped_frame, .map_frame = downsampled_frame, .keypoints = keypoints};
-  } else {
-    const Vector3dVector downsampled_frame = voxel_down_sample(clipped_frame, config.voxel_size);
-    return {.filtered_frame = clipped_frame, .map_frame = std::nullopt, .keypoints = downsampled_frame};
+    Vector3dVector downsampled_frame = voxel_down_sample(clipped_frame, config.voxel_size * 0.5);
+    Vector3dVector keypoints = voxel_down_sample(downsampled_frame, config.voxel_size * 1.5);
+    return {.filtered_frame = std::move(clipped_frame),
+            .keypoints = std::move(keypoints),
+            .map_frame = std::move(downsampled_frame)};
   }
+  Vector3dVector keypoints = voxel_down_sample(clipped_frame, config.voxel_size);
+  return {.filtered_frame = std::move(clipped_frame), .keypoints = std::move(keypoints), .map_frame = {}};
 }
 
 } // namespace rko_lio::core
