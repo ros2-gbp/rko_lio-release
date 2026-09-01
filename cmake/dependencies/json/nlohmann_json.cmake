112eecd7674d134b5d54a5abcf189e1a3efa592c
Meher Malladi <rm.meher97@gmail.com>
2026-09-01 21:28:33 +0530

    Rebase from 'debian/kilted/rko_lio'

--- cmake/dependencies/json/nlohmann_json.cmake
+++
@@ -1,7 +0,0 @@
-FetchContent_Declare(
-  nlohmann_json
-  URL https://github.com/nlohmann/json/archive/refs/tags/v3.12.0.tar.gz
-      ${RKO_LIO_FETCHCONTENT_COMMON_FLAGS})
-FetchContent_MakeAvailable(nlohmann_json)
-
-mock_find_package_for_older_cmake(nlohmann_json)
--- rko_lio/python/rko_lio/dataloaders/utils/static_tf_tree.py
+++ rko_lio/python/rko_lio/dataloaders/utils/static_tf_tree.py
@@ -37,12 +37,10 @@ def create_static_tf_tree(bag):
         dict: { child_frame_id: (parent_frame_id, transform) }
     """
     tf_tree = {}
-    if not "/tf_static" in bag.topics:
+    if "/tf_static" not in bag.topics:
         return tf_tree
 
-    tf_static_connections = [
-        conn for conn in bag.connections if (conn.topic == "/tf_static")
-    ]
+    tf_static_connections = [conn for conn in bag.connections if (conn.topic == "/tf_static")]
     for _, _, rawdata in tqdm(
         bag.messages(connections=tf_static_connections),
         desc="/tf_static messages",
