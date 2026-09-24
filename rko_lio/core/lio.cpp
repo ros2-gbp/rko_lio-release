9f07ea773b2c2bc161dab6d8868d1ed25a8be899
Meher Malladi <rm.meher97@gmail.com>
2026-09-25 00:27:42 +0200

    Rebase from 'release/rolling/rko_lio'

--- rko_lio/core/lio.cpp
+++ rko_lio/core/lio.cpp
@@ -24,6 +24,7 @@
 
 #include <spdlog/spdlog.h>
 
+#include "deskew.hpp"
 #include "error.hpp"
 #include "lio.hpp"
 #include "preprocess_scan.hpp"
@@ -110,43 +111,6 @@ AccelFilterStep step_body_accel_filter(const BodyAccelKF& prev,
       .info = AccelInfo{.accel_mag_variance = accel_mag_variance, .local_gravity_estimate = local_gravity_estimate},
       .updated = BodyAccelKF{.mean = new_mean, .covariance = new_cov},
   };
-}
-
-// Refers every point to timestamps.max
-void deskew_scan(Vector3sVector& scan, const Timestamps& timestamps, const MotionPrior& motion) {
-  const Sophus::SE3s start2reference = relative_pose_at_time(motion, timestamps.max).inverse();
-  const Eigen::Matrix3s& start2reference_rotation = start2reference.so3().matrix();
-  const Eigen::Vector3s& start2reference_translation = start2reference.translation();
-
-  // to avoid numerical issues
-  const Scalar angular_speed = std::max(motion.angular_velocity.norm(), static_cast<Scalar>(1e-6));
-  const Scalar inverse_angular_speed = 1 / angular_speed;
-  const Eigen::Vector3s axis = motion.angular_velocity * inverse_angular_speed;
-
-  std::transform(scan.cbegin(), scan.cend(), timestamps.per_point.cbegin(), scan.begin(),
-                 [&](const Eigen::Vector3s& point, const Nsec timestamp) {
-                   const auto dt = to_seconds<Scalar>(timestamp - motion.start_time);
-                   const Scalar theta = angular_speed * dt;
-                   const Scalar sin_theta = std::sin(theta);
-                   const Scalar one_minus_cos_theta = 1 - std::cos(theta);
-                   const Eigen::Vector3s mean_linear_velocity = motion.linear_velocity + motion.acceleration * dt / 2;
-                   const Eigen::Vector3s K_times_p = axis.cross(point);
-                   const Eigen::Vector3s K_times_linear_velocity = axis.cross(mean_linear_velocity);
-                   // SE(3) exp about a fixed axis, applied to the point: R * p + V * rho
-                   // Rodrigues -> K = hat(axis), R = I + sin(theta) K + (1 - cos theta) K^2
-                   // left Jacobian -> V = I + ((1 - cos theta) / theta) K + ((theta - sin theta) / theta) K^2
-                   // rho = dt * mean_linear_velocity and theta = angular_speed * dt
-                   // dt cancels in V * rho
-                   const Eigen::Vector3s R_times_p =
-                       point + sin_theta * K_times_p + one_minus_cos_theta * axis.cross(K_times_p);
-                   const Eigen::Vector3s V_times_rho =
-                       dt * mean_linear_velocity +
-                       (one_minus_cos_theta * inverse_angular_speed) * K_times_linear_velocity +
-                       ((theta - sin_theta) * inverse_angular_speed) * axis.cross(K_times_linear_velocity);
-                   const Eigen::Vector3s point_at_start = R_times_p + V_times_rho;
-                   // and finally move to reference time
-                   return Eigen::Vector3s(start2reference_rotation * point_at_start + start2reference_translation);
-                 });
 }
 
 using LinearSystem = std::tuple<Eigen::Matrix6s, Eigen::Vector6s, Scalar>;
@@ -547,14 +511,10 @@ Vector3sVector LIO::register_scan(Vector3sVector scan, const Timestamps& timesta
 
 Vector3sVector
 LIO::register_scan(const Sophus::SE3s& extrinsic_lidar2base, Vector3sVector scan, const Timestamps& timestamps) {
-  if (extrinsic_lidar2base.log().norm() < NEGLIGIBLE_EXTRINSIC) {
-    return register_scan(std::move(scan), timestamps);
+  if (extrinsic_lidar2base.log().norm() >= NEGLIGIBLE_EXTRINSIC) {
+    transform_points(extrinsic_lidar2base, scan);
   }
-
-  transform_points(extrinsic_lidar2base, scan);
-  Vector3sVector filtered_scan = register_scan(std::move(scan), timestamps);
-  transform_points(extrinsic_lidar2base.inverse(), filtered_scan);
-  return filtered_scan;
+  return register_scan(std::move(scan), timestamps);
 }
 
 void LIO::restart() {
